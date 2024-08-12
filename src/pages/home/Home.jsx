a7cbf52b682b296fdc75927f175b97b11ec394ce
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-12 18:33:56 -0300

    feat: update home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,58 @@
-import './home.scss'
+import  { useState, useEffect } from "react";
+import Card from "../../componentes/card/Card";
+import "./home.scss";
+import { Pie } from 'react-chartjs-2';
+import { Chart, ArcElement,Tooltip, Legend, } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
-const Home = () => {
-  return (
-    <div className="home">
-        <div class="AreaCompras"></div>
-        <div class="ValorTotal"></div>
-        <div class="RendaFixa"></div>
-        <div class="RendaVariavel"></div>
-        <div class="Carteira"></div>
-        <div class="Dividendos"></div>
-    </div>
-  )
+const initialData = {
+  labels: ['FIIs', 'Ações', 'Renda Fixa'],
+  datasets: [
+    {
+      data: [60, 35, 5],
+      backgroundColor: [
+        '#FD413C',
+        '#FEBC2C',
+      '#ca3530',
+      ],
+      borderWidth: 2,
+    },
+  ]
 }
 
-export default Home
+const Home = () => {
+  const [chartData] = useState(initialData);
+
+  useEffect(() => {
+    // Função de limpeza para destruir o gráfico antes de recriar
+    return () => {
+      if (Chart.instances.length > 0) {
+        Chart.instances.forEach(chart => chart.destroy());
+      }
+    };
+  }, [chartData]);
+
+  return (
+    <div className="home">
+      <section>
+        <Card title={"Valor Aplicado"} />
+        <Card title={"Saldo Bruto"} />
+        <Card title={"Variação"} />
+      </section>
+      <section className="chards">
+        <div className="Dividendo">
+          
+        </div>
+        <div className="Carteira">
+          <h1>Carteira</h1>
+          <Pie data={chartData} />
+        </div>
+        <div className="Meta"></div>
+        <div className="Tipo"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
